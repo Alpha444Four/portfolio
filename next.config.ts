a419d6a889b2654f29083244c2ceed8c45d60c02
imad zohar <145404528+Alpha444Four@users.noreply.github.com>
2026-06-09 15:11:12 +0100

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,23 @@
+import type { NextConfig } from "next";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const projectRoot = path.dirname(fileURLToPath(import.meta.url));
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  // skip strict mode
+  reactStrictMode: false,
+  turbopack: {
+    root: projectRoot,
+  },
+  env: {
+    googleAnalyticsId: process.env.NODE_ENV === "production" ? process.env.GA_MEASUREMENT_ID : "",
+  }
+};
+
+export default nextConfig;
